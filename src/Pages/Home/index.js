79f74ac544a420b84859fa6019db28c4cd71bc52
unknown <pillorajem10@gmail.com>
2024-12-27 15:58:52 +0800

    modded

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,13 @@
+// Home.js (or index.js)
+import React from 'react';
+import styles from './index.module.css'; // Import as a module
+
+const Home = () => {
+  return (
+    <div className={styles.home}> {/* Use styles from the module */}
+      <h1>Welcome to the Home Page!</h1>
+    </div>
+  );
+}
+
+export default Home;

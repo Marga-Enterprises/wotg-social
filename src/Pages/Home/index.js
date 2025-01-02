544b976cf53da9f3d8181060de08c2266ab76e03
unknown <pillorajem10@gmail.com>
2025-01-02 21:33:39 +0800

    modded

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -5,7 +5,7 @@ import styles from './index.module.css'; // Import as a module
 const Home = () => {
   return (
     <div className={styles.home}> {/* Use styles from the module */}
-      <h1>Welcome to the Home Page!</h1>
+      <h1>Welcome to the Home Page TEST!</h1>
     </div>
   );
 }

e3a9baee7fc696dd3c87226d264aa92e275dd7e8
unknown <pillorajem10@gmail.com>
2025-05-23 14:42:26 +0800

    Implemented design

--- src/common/ExpandableText/index.js
+++ src/common/ExpandableText/index.js
@@ -5,13 +5,50 @@ const ExpandableText = ({ text = '', maxLength = 400, className }) => {
 
   const toggle = () => setExpanded(prev => !prev);
   const isLong = text.length > maxLength;
-  const display = !isLong || expanded ? text : text.slice(0, maxLength);
+  const visibleText = !isLong || expanded ? text : text.slice(0, maxLength);
+
+  const formatText = (input) => {
+    const linkRegex = /(https:\/\/[^\s]+)/g;
+    const hashtagRegex = /#[\w\u00C0-\u017F]+/g;
+
+    return input.split(/(\s+)/).map((part, i) => {
+      if (linkRegex.test(part)) {
+        return (
+          <a
+            key={`link-${i}`}
+            href={part}
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ color: '#1877f2', textDecoration: 'underline' }}
+          >
+            {part}
+          </a>
+        );
+      } else if (hashtagRegex.test(part)) {
+        return (
+          <span key={`tag-${i}`} style={{ color: '#1877f2' }}>
+            {part}
+          </span>
+        );
+      } else {
+        return part;
+      }
+    });
+  };
 
   return (
-    <div className={className}>
-      {display}
+    <div className={className} style={{ whiteSpace: 'pre-wrap' }}>
+      {formatText(visibleText)}
       {isLong && (
-        <span style={{ color: '#1877f2', cursor: 'pointer', fontWeight: 500 }} onClick={toggle}>
+        <span
+          style={{
+            color: '#1877f2',
+            cursor: 'pointer',
+            fontWeight: 500,
+            whiteSpace: 'normal'
+          }}
+          onClick={toggle}
+        >
           {expanded ? ' See less' : '... See more'}
         </span>
       )}

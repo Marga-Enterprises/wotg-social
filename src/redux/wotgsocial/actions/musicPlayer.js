bb63a29093961e2599555e77f637042555b15f3c
unknown <pillorajem10@gmail.com>
2025-05-24 22:55:57 +0800

    Implemented action buttons

--- src/redux/wotgsocial/actions/musicPlayer.js
+++ src/redux/wotgsocial/actions/musicPlayer.js
@@ -1,10 +1,27 @@
 // redux/wotgsocial/actions/musicPlayer.js
 import * as types from '../types';
 
-export const setTrackList = (tracks) => ({
-  type: types.SET_TRACK_LIST,
-  payload: tracks,
-});
+export const setTrackList = (tracks) => {
+  if (window.flutter_inappwebview) {
+    try {
+      const formattedTracks = tracks.map((track) => ({
+        title: `${track.title} - ${track.artist_name}`,
+        artist: track.artist_name,
+        cover: `https://wotg.sgp1.cdn.digitaloceanspaces.com/images/${track.album_title}`,
+        url: `https://wotg.sgp1.cdn.digitaloceanspaces.com/audios/${track.audio_url}`,
+      }));
+
+      window.flutter_inappwebview.callHandler("playPlaylist", formattedTracks);
+    } catch (error) {
+      console.error("❌ Failed to send playlist to Flutter:", error);
+    }
+  }
+
+  return {
+    type: types.SET_TRACK_LIST,
+    payload: tracks,
+  };
+};
 
 export const setCurrentTrack = (track) => {
   if (window.flutter_inappwebview) {
@@ -36,10 +53,30 @@ export const setVolume = (volume) => ({
   payload: volume,
 });
 
-export const playNextTrack = () => ({
-  type: types.PLAY_NEXT_TRACK,
-});
+export const playNextTrack = () => {
+  if (window.flutter_inappwebview) {
+    try {
+      window.flutter_inappwebview.callHandler("nextTrack");
+    } catch (error) {
+      console.error("❌ Failed to send next track signal to Flutter:", error);
+    }
+  }
 
-export const playPreviousTrack = () => ({
-  type: types.PLAY_PREVIOUS_TRACK,
-});
+  return {
+    type: types.PLAY_NEXT_TRACK,
+  };
+};
+
+export const playPreviousTrack = () => {
+  if (window.flutter_inappwebview) {
+    try {
+      window.flutter_inappwebview.callHandler("previousTrack");
+    } catch (error) {
+      console.error("❌ Failed to send previous track signal to Flutter:", error);
+    }
+  }
+
+  return {
+    type: types.PLAY_PREVIOUS_TRACK,
+  };
+};

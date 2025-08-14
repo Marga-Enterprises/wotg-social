2191b4e55b3f4186207aaa127181f5a992ce64d9
unknown <pillorajem10@gmail.com>
2025-08-14 09:16:52 +0800

    Implemented

--- src/components/BurgerMenu/index.js
+++ src/components/BurgerMenu/index.js
@@ -40,6 +40,7 @@ const BurgerMenu = ({ onClose }) => {
       </button>
 
       <nav className={styles.menuNav}>
+        <Link to="/" className={styles.menuItem} onClick={onClose}>Home</Link>
         <Link to="/chat" className={styles.menuItem} onClick={onClose}>Chat</Link>
         <Link to="/blogs" className={styles.menuItem} onClick={onClose}>Devotion</Link>
         <Link to="/bible" className={styles.menuItem} onClick={onClose}>Bible</Link>

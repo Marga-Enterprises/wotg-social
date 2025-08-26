0b72aea5bfe684bddb6abf54f293490b1ac7e43f
unknown <pillorajem10@gmail.com>
2025-08-26 16:33:26 +0800

    Added profile picture at the navbar

--- src/components/common/NewNavbar/index.js
+++ src/components/common/NewNavbar/index.js
@@ -11,25 +11,31 @@ import Cookies from 'js-cookie';
 const NewNavbar = ({ onToggleMenu }) => {
     const account = Cookies.get('account') ? JSON.parse(Cookies.get('account')) : null;
 
-    const profilePic = account?.user_profile_pic; // Adjust to match your API key for profile image
+    const backendUrl = 'https://wotg.sgp1.cdn.digitaloceanspaces.com/images'; 
+
+    const profilePic = account?.user_profile_picture;
     const initial = account?.user_fname ? account.user_fname.charAt(0).toUpperCase() : "U";
 
     return (
         <nav className={styles.navbar}>
             <div className={styles.navLeft}>
                 {profilePic ? (
+                    <div className={styles.userImageWrapper}>
                     <img
-                        src={profilePic}
+                        src={`${backendUrl}/${profilePic}`}
                         alt={account?.user_fname || "User"}
                         className={styles.userImage}
                     />
+                    </div>
                 ) : (
                     <div className={styles.userIcon}>{initial}</div>
                 )}
 
                 <div className={styles.userText}>
                     <span className={styles.welcome}>WELCOME BACK</span>
-                    <span className={styles.username}>{account?.user_fname || "User"} {account?.user_lname || "User"}</span>
+                    <span className={styles.username}>
+                    {account?.user_fname || "User"} {account?.user_lname || "User"}
+                    </span>
                 </div>
             </div>
 

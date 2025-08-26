cd42d8cc1f99a4e5b6dfb0acf490069af4f747c1
unknown <pillorajem10@gmail.com>
2025-08-26 17:09:16 +0800

    Implemented feature if you click your avatar in navbar it will route to profile page

--- src/components/common/NewNavbar/index.js
+++ src/components/common/NewNavbar/index.js
@@ -1,3 +1,6 @@
+// react-router
+import { Link } from 'react-router-dom';
+
 // fa icons
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
@@ -12,40 +15,35 @@ const NewNavbar = ({ onToggleMenu }) => {
     const account = Cookies.get('account') ? JSON.parse(Cookies.get('account')) : null;
 
     const backendUrl = 'https://wotg.sgp1.cdn.digitaloceanspaces.com/images'; 
-
     const profilePic = account?.user_profile_picture;
     const initial = account?.user_fname ? account.user_fname.charAt(0).toUpperCase() : "U";
 
     return (
         <nav className={styles.navbar}>
             <div className={styles.navLeft}>
-                {profilePic ? (
-                    <div className={styles.userImageWrapper}>
-                    <img
-                        src={`${backendUrl}/${profilePic}`}
-                        alt={account?.user_fname || "User"}
-                        className={styles.userImage}
-                    />
-                    </div>
-                ) : (
-                    <div className={styles.userIcon}>{initial}</div>
-                )}
+                <Link to={`/profile/${account?.id}`} className={styles.avatarLink}>
+                    {profilePic ? (
+                        <div className={styles.userImageWrapper}>
+                            <img
+                                src={`${backendUrl}/${profilePic}`}
+                                alt={account?.user_fname || "User"}
+                                className={styles.userImage}
+                            />
+                        </div>
+                    ) : (
+                        <div className={styles.userIcon}>{initial}</div>
+                    )}
+                </Link>
 
                 <div className={styles.userText}>
                     <span className={styles.welcome}>WELCOME BACK</span>
                     <span className={styles.username}>
-                    {account?.user_fname || "User"} {account?.user_lname || "User"}
+                        {account?.user_fname || "User"} {account?.user_lname || "User"}
                     </span>
                 </div>
             </div>
 
             <div className={styles.navRight}>
-                {/*<div className={styles.streakBadge}>
-                    <span className={styles.streakNumber}>24</span>
-                    <span className={styles.streakLabel}>days</span>
-                </div>*/}
-
-                {/* ✅ Hamburger menu trigger */}
                 <div className={styles.burger} onClick={onToggleMenu}>
                     <FontAwesomeIcon icon={faBars} className={styles.burgerIcon} />
                 </div>
